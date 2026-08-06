67cf0e2a325a2e53c54e5c15a922d58b2322a4db
Drake Natividad <drakedylannatividad17@gmail.com>
2026-08-07 06:29:24 +0800

    first commit
    B

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Information</title>
+</head>
+<body>
+    <h1>Welcome to My Laravel Project</h1>
+
+    <h2>Student Information</h2>
+
+    <p><strong>Student Name:</strong> Drake Dylan R. Natividad</p>
+    <p><strong>Student Number:</strong> 0124-0409</p>
+    <p><strong>Course:</strong> BS Information Technology</p>
+    <p><strong>Section:</strong> 3B</p>
+    <p><strong>Subject:</strong> ITST 302</p>
+    <p><strong>Current Date:</strong> August 6, 2026</p>
+
+</body>
+</html>
